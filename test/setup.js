45b2071fbd943cf0475faf2ba1abc05ef4c2cfb9
marko <marko@blindnet.io>
2021-04-23 20:19:38 +0200

    Init testing

---
+++ test/setup.js
@@ -0,0 +1,3 @@
+// node does not support atob and btoa
+global.atob = b64Encoded => Buffer.from(b64Encoded, 'base64').toString('binary')
+global.btoa = str => Buffer.from(str, 'binary').toString('base64')
