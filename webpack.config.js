890254623ba1979e0330d868ba0dcbf9930406f4
marko <marko@blindnet.io>
2021-05-06 17:05:54 +0200

    Prepare for npm publish

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'production',
+  devtool: false,
   entry: './src/index.ts',
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'lib'),
     library: {
       name: 'blindnet',
       type: 'umd'
@@ -27,7 +29,10 @@ module.exports = {
     ]
   },
   resolve: {
-    fallback: { crypto: false },
-    extensions: ['.ts', '.js']
-  }
-}
+    extensions: ['.ts', '.js'],
+    fallback: { crypto: false }
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
+};
